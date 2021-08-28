c731ce962896673bacc07d6aef6d7e9bca81fcf9
var1a3le <darklight1100@gmail.com>
2021-08-28 11:30:23 +0700

    testcase

---
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -0,0 +1,102 @@
+package badcode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class speakerImpl implements SpeakerRepository {
+    @Override
+    public Integer saveSpeaker(Speaker speaker) {
+        return 2;
+    }
+}
+
+class RegisterBusinessTest {
+    @Test
+    @DisplayName("Invalid first name")
+    public void case01() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        try {
+            registerBusiness.register(null, new Speaker());
+            fail();
+        } catch (ArgumentNullException e) {
+            //
+        }
+    }
+
+    @Test
+    @DisplayName("Invalid last name")
+    public void case02() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker mock = new Speaker();
+        mock.setFirstName("FirstName");
+        try {
+            registerBusiness.register(null, mock);
+            fail();
+        } catch (ArgumentNullException e) {
+            //
+        }
+    }
+
+    @Test
+    @DisplayName("Invalid email")
+    public void case03() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker mock = new Speaker();
+        mock.setFirstName("FirstName");
+        mock.setLastName("Lastname");
+        try {
+            registerBusiness.register(null, mock);
+            fail();
+        } catch (ArgumentNullException e) {
+            //
+        }
+    }
+
+    @Test
+    @DisplayName("Domain invalid")
+    public void case04() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker mock = new Speaker();
+        mock.setFirstName("FirstName");
+        mock.setLastName("Lastname");
+        mock.setEmail("test@test.com");
+        try {
+            registerBusiness.register(null, mock);
+            fail();
+        } catch (SpeakerDoesntMeetRequirementsException e) {
+            //
+        }
+    }
+
+    @Test
+    @DisplayName("Cannot Save")
+    public void case05() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker mock = new Speaker();
+        mock.setFirstName("FirstName");
+        mock.setLastName("Lastname");
+        mock.setEmail("test@gmail.com");
+        mock.setExp(2);
+        try {
+            registerBusiness.register(null, mock);
+            fail();
+        } catch (SaveSpeakerException e) {
+            //
+        }
+    }
+
+    @Test
+    @DisplayName(" Save")
+    public void case06() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker mock = new Speaker();
+        mock.setFirstName("FirstName");
+        mock.setLastName("Lastname");
+        mock.setEmail("test@gmail.com");
+        mock.setExp(2);
+        int actual = registerBusiness.register(new speakerImpl(), mock);
+        assertEquals(2, actual);
+    }
+}
